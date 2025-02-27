053e79ff9ce2ca6427b27a401560da47f491527e
Alexis Pister <alexis.pister@hotmail.com>
2025-02-27 14:56:01 +0000

    Updates

---
+++ _app/immutable/chunks/paths.bnOphAGh.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1s872vk?.base??"",a=globalThis.__sveltekit_1s872vk?.assets??s;export{a,s as b};
---
+++ _app/immutable/entry/start.BFlGlhuE.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Chnn_uRI.js";export{t as start};
