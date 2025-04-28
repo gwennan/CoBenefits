0529989052b0cec2b92adf4066a467e92b28cf1d
Alexis Pister <alexis.pister@hotmail.com>
2025-04-28 14:04:00 +0100

    Updates

--- _app/immutable/chunks/paths.BWl5fsg7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1p55ofs?.base??"",a=globalThis.__sveltekit_1p55ofs?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/paths.CePn39rv.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1u07hel?.base??"",e=globalThis.__sveltekit_1u07hel?.assets??s;export{e as a,s as b};
