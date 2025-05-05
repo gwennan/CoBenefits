4afd696401a2eebab413f3feb00f7ca30907c173
sian-phillips-1 <sian.phillips00@yahoo.com>
2025-05-05 16:08:51 +0100

    fixing se labels

--- src/globals.ts
+++ src/globals.ts
@@ -111,7 +111,8 @@ export const SEF_LEVEL_LABELS = {
       1: "Owner", 2: "Rented (social)", 3: "Rented (private)"
     },
     Typology: {
-      1: "Semi-detached", 2: "Detached", 3: "Mid-terrace", 4: "End-terrace", 5: "Enclosed end-terrace", 6: "Enclosed mid-terrace"
+      1: "Semi-detached", 2: "Detached", 3: "Mid-terrace", 
+      // 4: "End-terrace", 5: "Enclosed end-terrace", 6: "Enclosed mid-terrace"
     },
     Fuel_Type: {
       1: "Gas boiler", 2: "Electric heating", 3: "Oil heating"
