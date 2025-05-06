1172e91b6f05d21cffa5876b6e78cc5a1c2d513d
Jinrui Wang <jinrui.w@outlook.com>
2025-05-06 12:31:54 +0100

    Updates

---
+++ _app/immutable/chunks/D-bP4ZYy.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1yub0np?.base??"",a=globalThis.__sveltekit_1yub0np?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.CPiWD8rj.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ax9p8e?.base??"",a=globalThis.__sveltekit_ax9p8e?.assets??s;export{a,s as b};
