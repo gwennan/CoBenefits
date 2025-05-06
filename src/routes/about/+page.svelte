9706616b761a63e0bcace6f7a9bc11e97b35d563
benjbach <benj.bach@gmail.com>
2025-05-06 09:28:11 +0200

    update new about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,7 +12,7 @@
     </section>
   
     <section class="about-intro">
-      <p class="intro-paragraph">
+      <!-- <p class="intro-paragraph">
         Climate actions are designed to lower greenhouse gas (GHG) emissions but the gains for society reach further. For example, electric cars reduce air pollution, retrofitting homes minimises cold, damp and mould growth, and our health outcomes in turn improve.
         </p>
       
@@ -26,17 +26,116 @@
 
       <p class="intro-paragraph">
         Explore this data atlas to further understand connections between a wide range of social, economic and environmental priorities and drive more effective decision-making. 
-      </p>
+      </p> -->
+
+      <h2>
+        In a nutshell
+      </h2>
+
+<p class="intro-paragraph">
+The UK Co-Benefits Atlas is an interactive visual interface for communicating the co-benefits of reaching net zero across the UK. The overarching aim of this project is to support stakeholders in the making of a just and sustainable net zero United Kingdom. 
+</p>
+<p class="intro-paragraph">
+  Climate actions are designed to lower greenhouse gas (GHG) emissions but the gains for society reach further. For example, electric cars reduce air pollution, retrofitting homes minimises cold, damp and mould growth, and our health outcomes in turn improve.
+</p>
+<p class="intro-paragraph">
+    The CO-BENS project models 11 additional benefits for the climate actions recommended by the Climate Change Committee (CCC) in its Seventh Carbon Budget (2025) to assist the United Kingdom with meeting its net zero target.
+  </p>
+    <p class="intro-paragraph">
+      For 46,000 communities and regions across the UK, calculations consider localised context to 
+determine actions that can be implemented in addition to how, when and for whom benefits will emerge (or in some cases costs).
+</p>
+
+<p class="intro-paragraph">
+Explore this data atlas to further understand connections between a wide range of social, economic and environmental priorities and drive more effective decision-making.
+</p>
+
+<p class="intro-paragraph">
+  Join our mailing list to receive updates on future features and events about the atlas: https://groups.google.com/g/ukcobenefitsatlas
+</p>
+
+<h3>Atlas citation</h3>  
+<p class="citation-text">
+    <i>Wang, J; Pister, A; Phillips, S, Hinrichs, U; Field, S; Sudmant, A; Bissett, S; Wharmby, C; Higgins-Lavery, R.; Bach, B (2025). “The UK Co-Benefits Atlas”. Published May 6, 2025.</i>
+</p>
+
+
+<p class="intro-paragraph">
+  The atlas received funding from Scotland Beyond Net Zero (https://scotland-beyond-net-zero.ac.uk). 
+</p>
+
+<h2>What are Co-Benefits?</h2>
+
+<p class="intro-paragraph">
+Current research from the Edinburgh Climate Change Institute (ECCI), using a model developed for the UK Climate Change Committee (CCC), finds that for every 1£ spent on climate change mitigation in the UK there are as much as 14£ of social benefits in the form of improved public health, better urban connectivity, and increased productivity.
+</p>
+<p class="intro-paragraph">
+The ECCI modelling team provides 11 co-benefits for >1000 possible climate interventions in ~46,000 UK 'datazones' (lower-layer super output areas,  LSOAs) across the UK. The UKCBA continues  to co-create  this interactive and increasingly data-rich and engaging visualization atlas with its stakeholders; for this project we already have strong commitment from Scottish partners across academia, business, and the public sector (see below), stating the need of such an atlas and which are highly invested in supporting the project through attending workshops and helping with its co-design. The end result will be a collaboratively created, cutting-edge, and first-of-its-kind platform capable of facilitating interdisciplinary and cross-sectoral collaborations and decision making between diverse stakeholders around the complex socio-economic benefits of climate interventions.
+</p>
+
+<h3>Co-benefits citation</h3> 
+<p class="intro-paragraph">
+<i>[1] Sudmant, A., Boyle, D., Higgins‐Lavery, R., Gouldson, A., Boyle, A., Fulker, J., & Brogan, J. (2024). Climate policy as social policy? A comprehensive assessment of the economic impact of climate action in the UK. Journal of Environmental Studies and Sciences, 1-15.</i>
+</p>
+
+<h2>Who are the target audiences?</h2>
+
+
+<p class="intro-paragraph">
+  Accurately capturing and communicating the benefits of place-based interventions is a key stumbling block of climate change policies in Scotland. Common framings around "fiscal spend" and limited pathways for economic returns, miss the broader economic value that net zero interventions can generate. With the open data and analyses published through our atlas, we aim to inform decision making  made by:
+</p>
+  <ul>
+    <li>Businesses seeking commercial net zero opportunities.</li> 
+    <li>Communities and policy makers with net zero mandates.</li> 
+    <li>Researchers and analysts examining the socio-economic co-benefits of net zero interventions.</li> 
+  </ul>
+
+<h2>What's a Visualization Atlas?</h2>
+
+<p class="intro-paragraph">
+Visualization atlases are online platforms making large and complex data sets accessible and understandable to wide audiences. To that end, they employ interactive visualizations and explanations. This project has been inspired by existing visualization atlases on, e.g., Sustainable Development Goals, the economic complexity of Our World in Data. Read more about the concept of visualization atlases and their key characteristics: 
+</p>
+  <ul>
+  <li>TOPICS: Visualization atlases present complex topics, targeting a wide range of audiences. Topics are complex, data-driven and comprehensive.</li>
+  <li>CURATION—Visualization atlases are highly curated in terms of content, data and presentation; they are scoped, structured, and visually curated.</li>
+  <li>VISUALIZATION: Visualization atlases heavily rely on data visualization; they are visualization-driven, explanatory and exploratory.</li>
+</ul>
+
+<h3>Visualization Atlases citation</h3>  
+<p class="intro-paragraph">
+  <i>[2] <a href="https://arxiv.org/pdf/2408.07483">Wang, J., Shu X., Bach B., Hinrichs U., (2024). Visualization Atlases: Explaining and Exploring Complex Topics through Data, Visualization, and Narration. Transactions on Visualization and Computer Graphics.</a></i>
+</p>
+
+
+<h2>Who's involved in this project?</h2>
+
+<h3>The co-benefits modeling team</h3>
+<ul>
+<li><a href="https://edinburghcentre.org/team/andrew-sudmant" target="_blank">Andrew Sudmant</a>, <i>University of Edinburgh</i></li>
+<li><a href="https://edinburghcentre.org/team/ruaidhri-higgins-lavery" target="_blank">Ruaidhri Higgins-Lavery</a>, <i>University of Edinburgh</i></li>
+<li><a href="https://www.climateintelligenceservice.scot/about-us/meet-the-team" target="_blank">Sarah Bissett</a>, <i>University of Edinburgh</i></li>
+<li><a href="https://energyethics.st-andrews.ac.uk/people/sean-field/" target="_blank">Sean Field</a>, <i>University of St Andrews</i></li>
+</ul>
+ 
+<h3>The visualization team</h3> 
+<p class="intro-paragraph">
+  The visualization team comprises researchers from the <a href="https://vishub.net">VisHub research</a> group at the School of Informatics, University of Edinburgh. 
+</p>
+
+<ul>
+  <li><a href="https://benjbach.net" target="_blank">Benjamin Bach</a>, <i>Inria Bordeaux & University of Edinburgh</i></li>
+    <li><a href="https://www.jinruiw.com/" target="_blank">Jinrui Wang</a>, <i>University of Edinburgh</i></li>
+      <li><a href="https://alexispister.github.io/" target="_blank">Alexis Pister</a>, <i>University of Edinburgh</i></li>
+        <li>Sian Phillips, <i>University of Edinburgh</i></li>
+          <li><a href="http://utahinrichs.de/" target="_blank">Uta Hinrichs</a>, <i>University of Edinburgh</i></li>
+</ul>
+
+
     </section>
   
 
   
-    <section class="about-citation">
-      <h2>Atlas Citation</h2>
-      <p class="citation-text">
-        Citation: Wang, J; Pister, A; Phillips, S, Hinrichs, U; Field, S; Sudmant, A; Bissett, S; Wharmby, C; Higgins-Lavery, R.; Bach, B (2025). “The UK Co-Benefits Atlas”. Published May 6, 2025.
-      </p>
-    </section>
+
 
     <section class="about-logos-wrapper">
         <div class="logos-grid">
