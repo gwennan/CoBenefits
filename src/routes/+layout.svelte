f40104341e259a14c6c8c0ae9cbb05a450fcbaf2
Alexis Pister <alexis.pister@hotmail.com>
2025-05-15 12:31:05 +0100

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,23 @@
     import '../app.css';
     import {onMount} from 'svelte';
     import {AVERAGE_COLOR} from "../globals";
-
+    import {browser} from '$app/environment'
 
     onMount(() => {
 		document.documentElement.style.setProperty('--compareColor', AVERAGE_COLOR);
 	});
+
+    if (browser) {
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function gtag(){dataLayer.push(arguments);}
+        window.gtag('js', new Date());
+        window.gtag('config', 'G-GGZ403XD90');
+    }
 </script>
 
 
 <slot/>
+
+<svelte:head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GGZ403XD90"></script>
+</svelte:head>
